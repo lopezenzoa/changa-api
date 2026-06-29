d41d257b1f0fedd8a14b05a868ec980c54fd73c2
lopezenzoa <enzoagustinlopez2003@gmail.com>
2026-06-29 13:30:13 -0300

    initial commit

---
+++ src/test/java/com/portfolio/changa_api/ChangaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.changa_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
