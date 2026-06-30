9984feaa2caac0dc41ba309335911bafc4babeaa
lopezenzoa <enzoagustinlopez2003@gmail.com>
2026-06-30 11:28:56 -0300

    fix: add both Equals and HashCode methods both Facility and User to model layer

--- src/main/java/com/portfolio/changa_api/model/Facility.java
+++ src/main/java/com/portfolio/changa_api/model/Facility.java
@@ -1,10 +1,7 @@
 package com.portfolio.changa_api.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
@@ -14,6 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @Entity
 @Table(name = "facilities")
+@EqualsAndHashCode
 public class Facility {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/portfolio/changa_api/model/User.java
+++ src/main/java/com/portfolio/changa_api/model/User.java
@@ -1,10 +1,7 @@
 package com.portfolio.changa_api.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
@@ -14,6 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
+@EqualsAndHashCode
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
